2721f38e83d9e35ce9206737e2ee7ae89e6babc3
swiftkeyy <80327899+swiftkeyy@users.noreply.github.com>
2026-03-31 23:45:25 +0400

    Delete cleanup_tracked_files.sh

--- cleanup_tracked_files.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-git rm --cached bot.db bot.log test_bot.db || true
-rm -f bot.db bot.log test_bot.db
-git add .gitignore README_QUICKSTART.md config.py
-echo "Готово. Теперь сделай commit."
